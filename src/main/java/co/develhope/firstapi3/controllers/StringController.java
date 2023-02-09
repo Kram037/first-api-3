d0b83b85551d95d53961d404700dbfeeadb58147
Kram037 <defenzomarco@gmail.com>
2023-02-09 12:12:14 +0100

    Fix: fixed mapping

--- src/main/java/co/develhope/firstapi3/controllers/StringController.java
+++ src/main/java/co/develhope/firstapi3/controllers/StringController.java
@@ -3,9 +3,10 @@ package co.develhope.firstapi3.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/strings")
 public class StringController{
 
-    @GetMapping("/strings/{str}")
+    @GetMapping("/{str}")
     public String getString(@PathVariable String str, @RequestParam(required = false) String str1){
 
         if(str1 == null){
